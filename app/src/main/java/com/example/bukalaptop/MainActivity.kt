5d9b363b1bbe292310829626632479ac3534a9d6
Izznakim <IzzuddinNaufalHakim@gmail.com>
2023-06-30 16:07:07 +0700

    Halaman pemilihan pegawai dan pelanggan selesai.

--- app/src/main/java/com/example/bukalaptop/MainActivity.kt
+++ app/src/main/java/com/example/bukalaptop/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.bukalaptop
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnPegawai: Button
+    private lateinit var btnPelanggan: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnPegawai = findViewById(R.id.btn_pegawai)
+        btnPelanggan = findViewById(R.id.btn_pelanggan)
+
+        btnPelanggan.setOnClickListener {
+            val pelangganIntent = Intent(this, PelangganActivity::class.java)
+            startActivity(pelangganIntent)
+        }
+        btnPegawai.setOnClickListener {
+            val pegawaiIntent = Intent(this, PegawaiActivity::class.java)
+            startActivity(pegawaiIntent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/bukalaptop/PegawaiActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bukalaptop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PegawaiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pegawai)
+    }
+}
