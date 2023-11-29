052f0a718cd558cb6ebac49e85997effc82d856e
Izznakim <IzzuddinNaufalHakim@gmail.com>
2023-11-29 14:49:00 +0700

    Menampilkan data barang ke Halaman barang bagian pelanggan.

--- app/src/main/java/com/example/bukalaptop/pelanggan/barang/BarangPelangganFragment.kt
+++ app/src/main/java/com/example/bukalaptop/pelanggan/barang/BarangPelangganFragment.kt
@@ -1,34 +1,24 @@
 package com.example.bukalaptop.pelanggan.barang
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.bukalaptop.R
+import com.example.bukalaptop.pegawai.barang.adapter.ListBarangAdapter
+import com.example.bukalaptop.pegawai.barang.model.Barang
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [BarangPelangganFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class BarangPelangganFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var rvBarang: RecyclerView
+    private lateinit var listBarangAdapter: ListBarangAdapter
+    private lateinit var listBarang: ArrayList<Barang>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +28,33 @@ class BarangPelangganFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_barang_pelanggan, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BarangPelangganFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BarangPelangganFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        rvBarang = view.findViewById(R.id.rv_barang)
+        rvBarang.setHasFixedSize(true)
+
+        initAdapter()
+
+        val db = Firebase.firestore
+        listBarang = arrayListOf()
+        db.collection("barang").addSnapshotListener { value, error ->
+            listBarang.clear()
+            if (value != null) {
+                for (document in value) {
+                    val barang = document.toObject(Barang::class.java)
+                    listBarang.add(barang)
                 }
+            } else if (error != null) {
+                Log.d("List Barang", error.toString())
             }
+            listBarangAdapter.setData(listBarang)
+        }
+    }
+
+    private fun initAdapter() {
+        rvBarang.layoutManager = LinearLayoutManager(activity)
+        listBarangAdapter = ListBarangAdapter(arrayListOf())
+        rvBarang.adapter = listBarangAdapter
     }
 }
